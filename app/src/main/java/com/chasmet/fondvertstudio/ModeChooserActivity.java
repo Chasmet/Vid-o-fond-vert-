840333b42e2cbb5419e0c212bd4faea20a102c4f
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-08-19 14:44:58 +0200

    Add launch mode chooser

---
+++ app/src/main/java/com/chasmet/fondvertstudio/ModeChooserActivity.java
@@ -0,0 +1,24 @@
+package com.chasmet.fondvertstudio;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+
+public final class ModeChooserActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mode_chooser);
+
+        MaterialButton classicButton = findViewById(R.id.classicModeButton);
+        MaterialButton musicButton = findViewById(R.id.musicModeButton);
+
+        classicButton.setOnClickListener(v ->
+                startActivity(new Intent(this, MainActivity.class)));
+        musicButton.setOnClickListener(v ->
+                startActivity(new Intent(this, ClipMusicActivity.class)));
+    }
+}
